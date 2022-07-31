c0e43efa630d408575320e14c745216e0828ea7d
Jacob Gabriel <floodedcodeboy@gmail.com>
2022-07-31 22:31:06 +0100

    create feature context class

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -4,21 +4,11 @@ use Behat\Behat\Context\Context;
 
 use function PHPUnit\Framework\assertEquals;
 
-/**
- * Defines application features from the specific context.
- */
 class FeatureContext implements Context
 {
     private $stringCalculator;
     private $textToAdd;
 
-    /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
-     */
     public function __construct()
     {
     }
