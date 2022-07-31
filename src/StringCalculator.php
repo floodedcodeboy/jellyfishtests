f2e8e0c57f1d6e19525424b99292045d78ab8272
Jacob Gabriel <floodedcodeboy@gmail.com>
2022-07-31 22:51:43 +0100

    use custom deliminators in our string calculator

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -5,7 +5,6 @@ class StringCalculator
     private array $defaultDelimiters = ['|', ','];
 
     private string $delimiterInidicator = '//';
-
     /**
      * Returns the sum  of the numbers in the given string
      *
@@ -18,9 +17,29 @@ class StringCalculator
             return 0;
         }
 
-        $items = explode(',', $numbers);
-
+        $items = $this->splitNumbers($numbers);
         return count($items) == 1 ? $numbers : array_sum($items);
+    }
+
+    /**
+     * Splits the string of numbers based on internal delimiters
+     * if none are provided within the string.
+     *
+     * @param string $numbers
+     * @return array
+     */
+    protected function splitNumbers(string $numbers): array
+    {
+        $delimiters = $this->defaultDelimiters;
+        if (substr($numbers, 0, 2) === $this->delimiterInidicator) {
+            $customDelim = substr($numbers, 2, 1);
+            $needle = str_replace($this->delimiterInidicator . $customDelim, '', $numbers);
+            $numbers = str_replace("\\n", "", $needle);
+            $items = explode($customDelim, $numbers);
+        } else {
+            $items = preg_split('/[' . implode(',', $delimiters) . ']/', $numbers);
+        }
+        return $items;
     }
 
 }
